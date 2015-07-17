f405acaf2f2c413e425dbafbf46694bf962f9086
Jose Martinez Ruiz <jmmrcp@gmail.com>
2015-07-17 14:35:46 +0200

    Primeras pruebas OK

--- Drop/core/src/com/badlogic/drop/Drop.java
+++ Drop/core/src/com/badlogic/drop/Drop.java
@@ -1,27 +1,75 @@
 package com.badlogic.drop;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
-public class Drop extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Drop implements ApplicationListener {
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Texture dropImage, bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+	private Rectangle bucket;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		//img = new Texture("badlogic.jpg");
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		bucket = new Rectangle();
+		bucket.x = 800 /2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.width = 64;
+
+
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		rainMusic.setLooping(true);
+		rainMusic.play();
+	}
+
+	@Override
+	public void resize(int i, int i1) {
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
 	}
 }
