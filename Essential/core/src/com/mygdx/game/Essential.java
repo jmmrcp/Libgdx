950a5690589e8d86430753f4bf91833ae46526bd
jmmrcp <jmmrcp@gmail.com>
2015-08-07 11:17:49 +0200

    Overlap

--- Essential/core/src/com/mygdx/game/Essential.java
+++ Essential/core/src/com/mygdx/game/Essential.java
@@ -1,27 +1,24 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.uwsoft.editor.renderer.SceneLoader;
 
-public class Essential extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+public class Essential extends Game {
+	private SceneLoader sl;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		sl = new SceneLoader();
+		sl.loadScene("MainScene");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		sl.getEngine().update(Gdx.graphics.getDeltaTime());
 	}
 }
--- Essential/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Essential/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.Essential;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 480;
+		config.width = 800;
+		config.title = "Overlap";
 		new LwjglApplication(new Essential(), config);
 	}
 }
